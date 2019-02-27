97c2c005fec3c75940b9d322b4416a1927f9e27a
joakimed <joakim.dyngeland@hotmail.com>
2019-02-27 12:37:13 +0100

    Update fbchat-threading.py
    Printing now works

--- fbchat-threading.py
+++ fbchat-threading.py
@@ -8,6 +8,7 @@ import threading
 email = "<email>"
 pw = "<password>"
 
+
 def send():
     client = Client(email, pw)
 
@@ -19,8 +20,16 @@ def send():
             client.send(Message(text=payload), thread_id=client.uid, thread_type=ThreadType.USER)
 
 def receive():
-    client2 = Client(email, pw)     # We have to create a dedicated Client for listening,
-    client2.listen()                # while still being able to send messages
+    client2 = PrintMessage(email, pw)     # We have to create a dedicated Client for listening,
+    client2.listen()                      # while still being able to send messages
+
+
+class PrintMessage(Client):
+    def onMessage(self, author_id, message_object, thread_id, thread_type, **kwargs):
+        self.markAsDelivered(thread_id, message_object.uid)
+        self.markAsRead(thread_id)
+
+        print("Incoming message: " + message_object.text)
 
 
 # Creating and starting separate threads for handling receiving and sending messages
@@ -28,10 +37,3 @@ t1 = threading.Thread(target=receive)
 t2 = threading.Thread(target=send)
 t1.start()
 t2.start()
-
-class PrintMessage(Client):
-    def onMessage(self, author_id, message_object, thread_id, thread_type, **kwargs):
-        self.markAsDelivered(thread_id, message_object.uid)
-        self.markAsRead(thread_id)
-
-        print("Incoming message: " + message_object.text) # This does not work for some reason
